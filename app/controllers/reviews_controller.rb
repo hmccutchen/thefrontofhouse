eb3ed450ae7026a4f25c973253ba96717ec42127
hassan mccutchen <mccutchen.hassan@gmail.com>
2018-03-26 16:05:25 -0400

    set-up review form

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,38 +1,46 @@
 class ReviewsController < ApplicationController
 
-  before_action :set_review, only: [:update, :destroy, :edit, :show]
+ before_action :set_restaurant, only: [:new,:update, :show, :create, :edit]
+ before_action :set_review, only: [:update, :destroy, :edit, :show]
 
 
 
 
-  def index
-  end
+ def index
+ end
 
-  def new
-    @review = Review.new
-  end
+ def new
+  @review = Review.new
+end
 
-  def create
-    @review = Review.create(review_params)
-  end
+def create
+  @review = Review.new(review_params)
+  @review.restaurant = @restaurant
+  @review.user = current_user
+  @review.save
+end
 
-  def update
-    @review = Review.find(@restaurant_params[:id])
-  end
+def update
+  @review = Review.find(@restaurant_params[:id])
+end
 
-  def destroy
-  end
+def destroy
+end
 
-  private
+private
 
-  def set_review
-    @review = restaurant.reviews.find(params[:id])
-  end
+def set_review
+  @review = restaurant.reviews.find(params[:id])
+end
+
+def set_restaurant
+  @restaurant = Restaurant.find(params[:restaurant_id])
+end
 
 
-  def review_params
-    params.require (:review).permit(:name, :position, :tips, :location)
-  end
+def review_params
+  params.require (:review).permit(:name, :position, :tips, :location)
+end
 
 
 end
